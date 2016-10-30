b805113711eded36c110db3609ed5760b3116808
edwinperaza <edwinperaza87@gmail.com>
2016-10-30 00:08:34 -0300

    Use PUG to write HTL with javascript

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 //Import package
 var express = require("express");
 var app = express();
+app.set('view engine', 'pug');
 
 //Set an initial endpoint
 app.get("/",function(req, res){
-  res.send("Hello world");
+  // res.send("Hello world");
+  // File index.pug must be saved at "views"
+  res.render('index.pug', { title : "My first webpage with express and pug", message: "Hello world!!!"});
 })
 //Set port where our server will be listening
 app.listen(8080);
